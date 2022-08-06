fb58a26d26f80ad59c1b559680734fd64c86ff3f
Kamila Kowalczyk <kamila.kowalczyk@icloud.com>
2022-08-06 19:56:01 -0400

    Connect to DB

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './db/connect.js';
+
 
 // middleware
 import notFoundMiddleware from './middleware/not-found.js';
@@ -9,7 +11,6 @@ import errorHandlerMiddleware from './middleware/error-handler.js';
 
 
 app.get('/', (req, res) => {
-    throw new Error('Something broke!');
     res.send('Hello World!');
 })
 
@@ -18,6 +19,17 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5005;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL);
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        })
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+start()
