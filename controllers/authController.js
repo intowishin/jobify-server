24ae4919a33fa37c24bb33754c84b3545a71ef7f
Kamila Kowalczyk <kamila.kowalczyk@icloud.com>
2022-08-06 20:22:10 -0400

    Auth Structure

---
+++ controllers/authController.js
@@ -0,0 +1,13 @@
+const register = (req, res) => {
+    res.send('Register');
+}
+
+const login = (req, res) => {
+    res.send('Login');
+}
+
+const update = (req, res) => {
+    res.send('Update');
+}
+
+export { register, login, update };
--- server.js
+++ server.js
@@ -2,17 +2,25 @@ import express from 'express';
 const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
+
+// db and authenticate user
 import connectDB from './db/connect.js';
 
+// routers
+import authRoutes from './routes/authRoutes.js';
 
 // middleware
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
+
+app.use('/api/v1/auth', authRoutes);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
