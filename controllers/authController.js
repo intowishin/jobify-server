2b1bf5230c5009ab72f318ba456076aef0a0b28d
Kamila Kowalczyk <kamila.kowalczyk@icloud.com>
2022-08-10 15:40:39 -0400

    Basic Register Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,13 @@
+import User from "../models/User.js";
+
 const register = async (req, res) => {
-    res.send('Register');
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json({user});
+    }
+    catch (err) {
+        res.status(500).json({ msg:"There was an error registering the user" });
+    }
 }
 
 const login = async (req, res) => {
